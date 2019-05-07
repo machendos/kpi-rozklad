195f95fae80b02bab813a6d3bd9bee502b16ae69
Nikita Machekhin <nicitamac@gmail.com>
2019-05-07 18:19:59 +0300

    Update main file with bot abstraction

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const { JSDOM } = require('jsdom');
-const BotApi = require('node-telegram-bot-api');
 const querystring = require('querystring');
 const fetch = require('node-fetch');
 const hiddenParametres =  require(__dirname + '/hidden-parametres.json');
-const sshSP = require(__dirname + '/sp.json');
+const createBotInstance = require(__dirname + '/src/bot-interaction.js');
+
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 
 const querableParametres = groupName => querystring
   .stringify(Object
@@ -17,17 +18,19 @@ const querableParametres = groupName => querystring
 const kpiRozkladURI =
   'http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx';
 
-const bot = new BotApi(
-  sshSP.BOT_TOKEN,
-  { polling: true }
-);
+const bot = createBotInstance(TELEGRAM_TOKEN);
 
-bot.on('message', msg => {
+// bot.on('message', (msg) => {
+//   msg.sendMessage(msg.shatId, 'hell');
+// });
+
+bot('now', (id, data) => {
+  const group = data;
 
   fetch(kpiRozkladURI, {
     method: 'POST',
     headers: { 'content-type': 'application/x-www-form-urlencoded' },
-    body: querableParametres(msg.text)
+    body: querableParametres(group)
   })
     .then(res => res.text())
     .then(body => {
@@ -41,6 +44,6 @@ bot.on('message', msg => {
       ]
         .map(element => element.title);
 
-      bot.sendMessage(msg.chat.id, subjects.join('\n'));
+      bot.sendMessage(id, subjects.join('\n'));
     });
 });
